f8c213adca1e29222802001106d7cdea44ac532f
Nikolay <mizenko1998@gmail.com>
2018-12-27 15:39:18 +1000

    add linter fixxing code using linter by airbnb integration with travis

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const sayHello = () => {
-    const name = readlineSync.question('May I have you name?');
-    console.log(`Hello, ${name}!`);
-}
+export default () => {
+  const name = readlineSync.question('May I have you name?');
+  console.log(`Hello, ${name}!`);
+};
